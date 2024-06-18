25c82a460b11a0207acdc8c7964c4a508e57e2f5
widat <widat.it@gmail.com>
2024-06-18 19:11:05 +0700

    initialize

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "golang-micro-api/proto"
+
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/logger"
+)
+
+var (
+	serviceName = "golang-micro-api"
+	version     = "latest"
+)
+
+func main() {
+	service := micro.NewService(
+		micro.Name(serviceName),
+		micro.Version(version),
+	)
+	service.Init()
+
+	client := pb.NewGolangMicroProductService("golang-micro-product", service.Client())
+
+	req := &pb.CallRequest{
+		Name: "widat",
+	}
+
+	resp, err := client.Call(context.Background(), req)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	fmt.Println(resp.Msg)
+
+}
